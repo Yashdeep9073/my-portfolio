0426ab7d2fdfc97dce49e301097fe0e108073484
Yashdeep <166975335+Yashdeep9073@users.noreply.github.com>
2025-08-04 14:03:27 +0530

    assets-files

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section id='hero' className='relative overflow-hidden'>
       <div className='absolute top-0 left-0 z-10'>
-        <img src="/images/bg.png" alt="background" />
+        <img src={`${import.meta.env.BASE_URL}images/bg.png`} alt="background" />
       </div>
 
       <div className='hero-layout'>
